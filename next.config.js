919e41374ecc24563056934b2b3faf7486484068
Nikhil Swaminathan <nikhil@live.ca>
2022-11-23 02:18:19 -0800

    next 13

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['adamdemo-e4cd5d5b223944-staging.s3.ca-central-1.amazonaws.com','main.d39ke6gomzcf0r.amplifyapp.com'],
     remotePatterns: [
       {
         protocol: 'https',

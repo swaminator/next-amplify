0716a0c034fb71c87610a2c1ce611b0488bce9e2
Nikhil Swaminathan <nikhil@live.ca>
2022-11-23 01:56:30 -0800

    remote patterns configured

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'adamdemo-e4cd5d5b223944-staging.s3.ca-central-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'main.d39ke6gomzcf0r.amplifyapp.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+
 }
